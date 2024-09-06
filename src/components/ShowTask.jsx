4939bad1bdcbcfc59d22f2be09362a8e572a1397
Vimal Kumar Meena <99386630+Vimal-Meena@users.noreply.github.com>
2024-09-06 18:32:16 +0530

    React First Project TodoApp

---
+++ src/components/ShowTask.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function ShowTask({
+  tasklist,
+  setTasklist,
+  handleEdit,
+  handleDelete,
+}) {
+  return (
+    <section className='showTask'>
+      <div className='head'>
+        <div>
+          <span className='title'>Todo</span>
+          <span className='count'>{tasklist.length}</span>
+        </div>
+        <button onClick={() => setTasklist([])} className='clearAll'>
+          Clear All
+        </button>
+      </div>
+      <ul>
+        {tasklist.map((task) => (
+          <li key={task.id}>
+            <p>
+              <span className='name'>{task.name}</span>
+              <span className='time'>{task.time}</span>
+            </p>
+            <i
+              className='bi bi-pencil-square'
+              onClick={() => handleEdit(task.id)}
+            ></i>
+            <i
+              className='bi bi-trash'
+              onClick={() => handleDelete(task.id)}
+            ></i>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
